0c3d3922933ff32a3461eb703e24d6fae0cd7595
Yang Zhang <yaaang@gmail.com>
2016-09-09 11:28:07 -0700

    Update pydub for debuggability

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     'path.py==7.2',
     'pq==1.2',
     'psycopg2==2.6',
-    'pydub==0.10.0',
+    'pydub==0.16.5',
     # The 'security' extra is to deal with SSL errors.  See
     # <http://stackoverflow.com/a/30438722/43118>.
     'requests[security]==2.7.0',
